dbfb5f84aa72ab4987123a95933a897360fe070d
David Tomaschik <david@systemoverlord.com>
2022-11-19 22:52:49 -0800

    Improve

--- config/config.go
+++ config/config.go
@@ -5,7 +5,9 @@ package config
 //go:generate protoc --go_out=pb --go_opt=paths=source_relative config.proto
 
 import (
+	"fmt"
 	"io"
+	"strings"
 
 	"google.golang.org/protobuf/proto"
 
@@ -14,6 +16,10 @@ import (
 
 type Config struct {
 	pb.Config
+}
+
+type Tool struct {
+	*pb.Tool
 }
 
 func LoadConfigFromReader(r io.Reader) (*Config, error) {
@@ -26,4 +32,31 @@ func LoadConfigFromReader(r io.Reader) (*Config, error) {
 		return nil, err
 	}
 	return &cfg, nil
+}
+
+func (cfg *Config) GetToolMap() (map[string]Tool, error) {
+	res := make(map[string]Tool)
+	for _, t := range cfg.Tool {
+		wrapped := Tool{t}
+		for _, p := range t.Path {
+			p = strings.TrimLeft(p, "/")
+			if _, ok := res[p]; ok {
+				return nil, fmt.Errorf("duplicate path: %v", p)
+			}
+			res[p] = wrapped
+		}
+	}
+	return res, nil
+}
+
+func (t Tool) GetCacheName() string {
+	if len(t.Path) < 1 {
+		// This is unservable anyway
+		return ""
+	}
+	return strings.ReplaceAll(strings.TrimLeft(strings.TrimRight(t.Path[0], "/"), "/"), "/", "_")
+}
+
+func (t Tool) String() string {
+	return t.Name
 }
